07b73e49cbbda626fffda213f327db2998dbe809
Masaru124 <masarukaze041@gmail.com>
2025-01-03 13:42:55 +0530

    f

--- news/wsgi.py
+++ news/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+from waitress import serve
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'news.settings')
 
 application = get_wsgi_application()
+
+# Bind to Render's PORT environment variable
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    serve(application, host="0.0.0.0", port=port)
