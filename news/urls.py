222fbdea44591134bdc081d04caa9a4103d99078
Masaru124 <masarukaze041@gmail.com>
2025-01-12 12:23:36 +0530

    likes comments1

--- news/urls.py
+++ news/urls.py
@@ -3,8 +3,7 @@ from django.urls import path, include
 from newsletter.views import welcome
 
 urlpatterns = [
-    path('', welcome, name='welcome'),  # Welcome page
-    path('index/', include('newsletter.urls')),  # Include index and other newsletter URLs
+    path('', include('newsletter.urls')), 
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),  # Include user-related URLs
 ]
