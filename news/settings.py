787ba85082dcaff67bcd1a68711e1b33b4a1c9bf
Masaru124 <masarukaze041@gmail.com>
2025-02-18 17:52:59 +0530

    deployremove

--- news/settings.py
+++ news/settings.py
@@ -5,6 +5,7 @@ from os import getenv
 from dotenv import load_dotenv
 from urllib.parse import urlparse
 
+
 # Initialize environment variables
 env = environ.Env()
 
@@ -12,11 +13,15 @@ env = environ.Env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-# Production settings
-DEBUG = env.bool('DEBUG', default=False)
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env.bool('DEBUG', default=True)
+
+# Application definition
 
 ALLOWED_HOSTS = [
     'news-ni7g.onrender.com',
@@ -25,8 +30,6 @@ ALLOWED_HOSTS = [
     '54.191.253.12',
     '44.226.122.3',
     '127.0.0.1',
-    '.vercel.app',
-    '.now.sh',
 ]
 
 INSTALLED_APPS = [
@@ -42,7 +45,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -71,8 +73,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'news.wsgi.application'
 
-# Database configuration
 tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -85,6 +87,8 @@ DATABASES = {
 }
 
 # Password validation
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -101,13 +105,21 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Internationalization
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
+
 LANGUAGE_CODE = 'en-us'
+
 TIME_ZONE = 'UTC'
+
 USE_I18N = True
+
 USE_L10N = True
+
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
@@ -115,10 +127,13 @@ STATICFILES_DIRS = [
     BASE_DIR / 'newsletter/Static',
 ]
 
-# Vercel deployment settings
-WHITENOISE_MANIFEST_STRICT = False
-WHITENOISE_ALLOW_ALL_ORIGINS = True
+# Add WhiteNoise middleware for serving static files in production
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
+# Enable compression and caching support
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
